b5555ebc7f7fc84e0d3ec8902bcc9c7bd472bdb6
Andreas Thomas <dev@chronark.com>
2022-05-06 20:20:43 +0200

    test: percentiles

--- src/limit.test.ts
+++ src/limit.test.ts
@@ -3,6 +3,8 @@ import { Ratelimit } from "./ratelimiter.ts";
 import { assertEquals } from "https://deno.land/std@0.136.0/testing/asserts.ts";
 import { TestHarness } from "./test_utils.ts";
 import type { Ratelimiter } from "./types.ts";
+import * as hdr from "https://esm.sh/hdr-histogram-js";
+
 function assertBetween(n: number, interval: [number, number]): void {
   assertEquals(n >= interval[0], true, `${n} is lower than ${interval[0]}`);
   assertEquals(n <= interval[1], true, `${n} is larger than ${interval[1]}`);
@@ -88,17 +90,25 @@ async function run(
         .toString()
         .padStart(4, " ")}req/s`,
       async () => {
-        const h = new TestHarness(
+        const harness = new TestHarness(
           new Ratelimit({
             redis: Redis.fromEnv(),
             limiter: limiter(tc),
           })
         );
-        await h.attack(tc.rate * tc.load, attackDuration);
-        assertBetween(h.metrics.success, tc.expected);
-        console.log(
-          Object.values(h.latencies).map(({ start, end }) => end - start)
-        );
+        await harness.attack(tc.rate * tc.load, attackDuration);
+        assertBetween(harness.metrics.success, tc.expected);
+
+        const h = hdr.build();
+        for (const { start, end } of Object.values(harness.latencies)) {
+          const latency = end - start;
+          h.recordValue(latency);
+        }
+        console.log(h.summary); // { "p50": 123, ... , max: 1244, totalCount: 3 }
+
+        // console.log(
+        //   Object.values(harness.latencies).map(({ start, end }) => end - start)
+        // );
       }
     );
   }
