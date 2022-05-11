9b003e5fb8df9552449f1412c0164630ce587de9
Andreas Thomas <dev@chronark.com>
2022-05-11 11:43:40 +0200

    fix: ignore any

--- examples/cf-worker/src/index.ts
+++ examples/cf-worker/src/index.ts
@@ -5,6 +5,7 @@ import type { Bindings } from "bindings";
 export async function handleRequest(
   _request: Request,
   env: Bindings,
+  // @ts-ignore
   context: any,
 ) {
   const redis = Redis.fromEnv(env);
