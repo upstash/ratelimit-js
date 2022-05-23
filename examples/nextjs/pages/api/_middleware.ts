f34742d8e03d77817085c8a8064825f99290b832
Andreas Thomas <dev@chronark.com>
2022-05-23 17:37:16 +0200

    chore: fix version

--- examples/nextjs/pages/api/_middleware.ts
+++ examples/nextjs/pages/api/_middleware.ts
@@ -2,24 +2,26 @@ import type { NextFetchEvent, NextRequest } from "next/server";
 import { MultiRegionRatelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
+const ratelimit = new MultiRegionRatelimit({
+  redis: [
+    new Redis({
+      url: process.env.UPSTASH_REDIS_REST_URL_FRA!,
+      token: process.env.UPSTASH_REDIS_REST_TOKEN_FRA!,
+    }),
+    new Redis({
+      url: process.env.UPSTASH_REDIS_REST_URL_IOWA!,
+      token: process.env.UPSTASH_REDIS_REST_TOKEN_IOWA!,
+    }),
+  ],
+  limiter: MultiRegionRatelimit.fixedWindow(10, "10 s"),
+});
+
 export default async function middleware(
   request: NextRequest,
   event: NextFetchEvent,
 ): Promise<Response | undefined> {
   const start = Date.now();
-  const ratelimit = new MultiRegionRatelimit({
-    redis: [
-      new Redis({
-        url: process.env.UPSTASH_REDIS_REST_URL_FRA!,
-        token: process.env.UPSTASH_REDIS_REST_TOKEN_FRA!,
-      }),
-      new Redis({
-        url: process.env.UPSTASH_REDIS_REST_URL_IOWA!,
-        token: process.env.UPSTASH_REDIS_REST_TOKEN_IOWA!,
-      }),
-    ],
-    limiter: MultiRegionRatelimit.fixedWindow(10, "10 s"),
-  });
+ 
 
   const ip = request.ip ?? "127.0.0.1";
 
