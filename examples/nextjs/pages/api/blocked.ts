be444e58f9a46d9d61b51bdb2199929685c576a9
Andreas Thomas <dev@chronark.com>
2022-08-22 10:17:46 +0200

    docs: update vercel edge example to use the new api (#13)

---
+++ examples/nextjs/pages/api/blocked.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  res.status(429);
+  return res.end();
+}
---
+++ examples/nextjs/pages/api/hello.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  return res.json({ hello: "world" });
+}
