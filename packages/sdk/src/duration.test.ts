b042dcba3989033d38c6af8cd40f065060fe4ab1
Andreas Thomas <dev@chronark.com>
2023-03-09 11:55:25 +0100

    feat: allow duration without space (#34)

---
+++ packages/sdk/src/duration.test.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from "@jest/globals";
+import { ms } from "./duration";
+
+describe("ms", () => {
+  it("should return the correct number of milliseconds for a given duration", () => {
+    expect(ms("100ms")).toBe(100);
+    expect(ms("2s")).toBe(2000);
+    expect(ms("3m")).toBe(180000);
+    expect(ms("4h")).toBe(14400000);
+    expect(ms("5d")).toBe(432000000);
+    expect(ms("10ms")).toBe(10);
+  });
+  describe("with space", () => {
+    it("should return the correct number of milliseconds for a given duration", () => {
+      expect(ms("100 ms")).toBe(100);
+      expect(ms("2 s")).toBe(2000);
+      expect(ms("3 m")).toBe(180000);
+      expect(ms("4 h")).toBe(14400000);
+      expect(ms("5 d")).toBe(432000000);
+      expect(ms("10 ms")).toBe(10);
+    });
+  });
+});
