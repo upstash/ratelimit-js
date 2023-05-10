ef1e755a693dfc839aa0f889584dae103239a100
The Command Runner <thecmdrunner@proton.me>
2023-05-10 17:04:14 +0530

    Update middleware.ts (#45)
    Make @typescript-eslint happy by importing types with `type` keyword.

--- examples/nextjs/middleware.ts
+++ examples/nextjs/middleware.ts
@@ -1,6 +1,6 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { type NextFetchEvent, type NextRequest, NextResponse } from "next/server";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
