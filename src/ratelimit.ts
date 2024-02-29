d22cf86b630e328ac1dddad639ba58ce0544be81
sourabpramanik <shubpramanik241@gmail.com>
2024-02-29 21:53:00 +0530

    feat: reset tokens API

--- src/ratelimit.ts
+++ src/ratelimit.ts
@@ -235,4 +235,39 @@ export abstract class Ratelimit<TContext extends Context> {
     }
     return res!;
   };
+
+  public resetUsedTokens = async (identifier: string) => {
+    const script = `
+      local pattern = KEYS[1]
+
+      -- Initialize cursor to start from 0
+      local cursor = "0"
+
+      repeat
+          -- Scan for keys matching the pattern
+          local scan_result = redis.call('SCAN', cursor, 'MATCH', pattern)
+
+          -- Extract cursor for the next iteration
+          cursor = scan_result[1]
+
+          -- Extract keys from the scan result
+          local keys = scan_result[2]
+
+          for i=1, #keys do
+          redis.call('DEL', keys[i])
+          end
+
+      -- Continue scanning until cursor is 0 (end of keyspace)
+      until cursor == "0"
+    `;
+    const pattern = [this.prefix, identifier, "*"].join(":");
+
+    if (Array.isArray(this.ctx.redis)) {
+      for (const db of this.ctx.redis) {
+        await db.eval(script, [pattern], [null]);
+      }
+    } else {
+      await this.ctx.redis.eval(script, [pattern], [null]);
+    }
+  };
 }
