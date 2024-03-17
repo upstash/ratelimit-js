3eb1f674fb04f5872466b2e063174c0fac355962
Hendrik Liebau <mail@hendrik-liebau.de>
2024-03-17 08:14:07 +0100

    docs: fix `@default` JSDoc comment for `analytics` config
    Syncing this with
    https://github.com/upstash/ratelimit/blob/3c3818daade86a2fa294cb4785535ecd981d706d/src/ratelimit.ts#L63.
    
    Potential follow-up: Maybe the three types `RatelimitConfig`,
    `RegionRatelimitConfig`, and `MultiRegionRatelimitConfig` should share a
    base config type that contains the common properties (`prefix`,
    `ephemeralCache`, `timeout`, and `analytics`).

--- src/single.ts
+++ src/single.ts
@@ -62,7 +62,7 @@ export type RegionRatelimitConfig = {
    * If enabled, the ratelimiter will store analytics data in redis, which you can check out at
    * https://console.upstash.com/ratelimit
    *
-   * @default true
+   * @default false
    */
   analytics?: boolean;
 };
