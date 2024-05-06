6b7feaaa9c7d990c6e98968b79847b3c2fdc695c
CahidArda <cahidardaooz@hotmail.com>
2024-05-06 20:03:28 +0300

    add details about ratelimit at vercel edge

--- examples/vercel-edge/app/api/route.ts
+++ examples/vercel-edge/app/api/route.ts
@@ -10,6 +10,7 @@ const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(10, "10 s"),
   prefix: "@upstash/ratelimit",
+  // analytics not enabled. See README for more details about analytics and Vercel Edge.
 });
 
 export async function GET(request: Request) {
