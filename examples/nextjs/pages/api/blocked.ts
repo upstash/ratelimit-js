e46ff1a72c3c2ee1006de03ef400f70d584c8fe0
CahidArda <cahidardaooz@hotmail.com>
2024-05-06 19:36:28 +0300

    re-write nextjs example with nextjs 14

--- examples/nextjs/pages/api/blocked.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(429);
-  return res.end();
-}
