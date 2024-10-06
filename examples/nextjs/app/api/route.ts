eefa3f95d37c33c53b7a987fe946343675773563
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-10-06 20:52:56 +0300

    fix: next.js example types deps

--- examples/nextjs/app/api/route.ts
+++ examples/nextjs/app/api/route.ts
@@ -6,9 +6,14 @@ import { waitUntil } from '@vercel/functions';
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+})
+
 // Create a new ratelimiter
 const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
+  redis,
   limiter: Ratelimit.slidingWindow(10, "10 s"),
   prefix: "@upstash/ratelimit",
   analytics: true
